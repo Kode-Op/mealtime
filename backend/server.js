c8bc81587c798299e0ed248bd0b4741eda76c14d
unknown <15david.allison@gmail.com>
2020-02-26 22:25:06 -0800

    Test Commit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
