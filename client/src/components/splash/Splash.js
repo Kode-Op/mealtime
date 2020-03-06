5687e6eb1db9c27a4cf0156f189e884e0a1e89af
Rachel Scherer <rscherer1@rccd.edu>
2020-03-06 13:17:56 -0800

    Added new splash image

--- client/src/components/splash/Splash.js
+++ client/src/components/splash/Splash.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import "./Splash.css";
-import splashplaceholder from "./splashplaceholder.jpg"
+import splashplaceholder from "./splashplaceholder.png";
 
 export default class Splash extends Component {
   render() {
-    return (
-      <img src={splashplaceholder} className="splashimg" width="100%"/>
-    );
+    return <img src={splashplaceholder} className="splashimg" width="100%" />;
   }
 }
