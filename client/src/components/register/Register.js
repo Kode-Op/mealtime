34c8372e7d641aba5b3a9f99ad9ea6223a1fa718
Rachel Scherer <rscherer1@student.rccd.edu>
2020-03-07 12:50:56 -0800

    Centered register div better

--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -116,63 +116,65 @@ export default class Register extends Component {
 
   render() {
     return (
-      <div className="registerContainer">
-        <form onSubmit={this.onSubmit}>
-          <h2 className="registerHeader">Create your account</h2>
-          <div className="registerDivSplitLeft">
-            <label htmlFor="fName">First name</label>
+      <div className="mainContainer">
+        <div className="registerContainer">
+          <form onSubmit={this.onSubmit}>
+            <h2 className="registerHeader">Create your account</h2>
+            <div className="registerDivSplitLeft">
+              <label htmlFor="fName">First name</label>
+              <input
+                type="text"
+                name="fName"
+                className="inputBox"
+                value={this.state.firstName}
+                onChange={this.onChangeFirstName}
+                required
+              />
+            </div>
+            <div className="registerDivSplitRight">
+              <label htmlFor="lName">Last name</label>
+              <input
+                type="text"
+                name="lName"
+                className="inputBox"
+                value={this.state.lastName}
+                onChange={this.onChangeLastName}
+                required
+              />
+            </div>
+            <label htmlFor="email">Email</label>
             <input
               type="text"
-              name="fName"
+              name="email"
               className="inputBox"
-              value={this.state.firstName}
-              onChange={this.onChangeFirstName}
+              value={this.state.email}
+              onChange={this.onChangeEmail}
               required
             />
-          </div>
-          <div className="registerDivSplitRight">
-            <label htmlFor="lName">Last name</label>
+            <label htmlFor="password">Password</label>
             <input
-              type="text"
-              name="lName"
+              type="password"
+              name="password"
               className="inputBox"
-              value={this.state.lastName}
-              onChange={this.onChangeLastName}
+              value={this.state.password}
+              onChange={this.onChangePassword}
               required
             />
-          </div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            name="email"
-            className="inputBox"
-            value={this.state.email}
-            onChange={this.onChangeEmail}
-            required
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            className="inputBox"
-            value={this.state.password}
-            onChange={this.onChangePassword}
-            required
-          />
-          <Button
-            variant="success"
-            className="registerButton"
-            type="submit"
-            block
-          >
-            Sign up
-          </Button>
-        </form>
-        <div className="registerErrorMessage">{this.state.errorMessage}</div>
-        Already have an account?
-        <Link to="/login" className="loginLink">
-          Log in!
-        </Link>
+            <Button
+              variant="success"
+              className="registerButton"
+              type="submit"
+              block
+            >
+              Sign up
+            </Button>
+          </form>
+          <div className="registerErrorMessage">{this.state.errorMessage}</div>
+          Already have an account?
+          <Link to="/login" className="loginLink">
+            Log in!
+          </Link>
+        </div>
       </div>
     );
   }
