8abfdd40ad0457cc7365bbe7bac05a8fe1fd0c08
Rachel Scherer <rscherer1@student.rccd.edu>
2020-03-09 21:19:57 -0700

    Changed splash screen and navbar

--- client/src/components/nav/Navbar.js
+++ client/src/components/nav/Navbar.js
@@ -1,36 +1,39 @@
 import React, { Component } from "react";
 import { Navbar, Nav, ButtonToolbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./Navbar.css"
+import "./Navbar.css";
 
 export default class NavBar extends Component {
   render() {
     return (
       <div>
-        <Navbar fixed="top" variant="light" className="navcontainer">
-          <Navbar.Brand>MealTime</Navbar.Brand>
+        <Navbar fixed="top" variant="light">
           <Nav.Link>
-            <Link to="/" className="linkstyle">Home</Link>
+            <Link to="/" className="linkstyle">
+              MealTime
+            </Link>
           </Nav.Link>
           <Nav.Link>
-            <Link to="/data" className="linkstyle">Data</Link>
+            <Link to="/data" className="linkstyle">
+              Data
+            </Link>
           </Nav.Link>
           <Navbar.Collapse className="justify-content-end">
             <ButtonToolbar>
               <Nav.Link>
-                <div className="loginbutton">
-                  <Link to="/login" className="linkstyle">Log In</Link>
-                </div>
+                <Link to="/login" className="linkstyle">
+                  Log In
+                </Link>
               </Nav.Link>
               <Nav.Link>
-                <div className="registerbutton">
-                  <Link to="/register" className="linkstyle">Register </Link>
-                </div>
+                <Link to="/register">
+                  <div className="registerbutton">Register</div>
+                </Link>
               </Nav.Link>
             </ButtonToolbar>
           </Navbar.Collapse>
         </Navbar>
-        </div>
+      </div>
     );
   }
 }
--- client/src/components/splash/Splash.js
+++ client/src/components/splash/Splash.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 import "./Splash.css";
 import splashplaceholder from "./splashplaceholdershadow.png";
 
 export default class Splash extends Component {
   render() {
     return (
-      <img src={splashplaceholder} className="splashimg" alt="" width="100%" />
+      <div className="splashcontainer">
+        <img src={splashplaceholder} alt="" />
+        <div className="splashzone">
+          <h1>Any time can be MealTime.</h1>
+          <form>
+            <input
+              type="text"
+              name="address"
+              className="splashbox"
+              placeholder="Enter your address..."
+            />
+            <Button className="splashgo" variant="danger">
+              >
+            </Button>
+          </form>
+        </div>
+      </div>
     );
   }
 }
