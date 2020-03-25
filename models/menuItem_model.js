7fda5858a25cbc52d681b7f616a5281ed3d8af00
unknown <15david.allison@gmail.com>
2020-03-24 18:24:58 -0700

    Finished menuItem API rework, added restaurantId, category

--- models/menuItem_model.js
+++ models/menuItem_model.js
@@ -1,18 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const menuItemSchema = new Schema({
-    id: {type: Number, required: true},
-    name: {type: String, required: true, unique: true, trim: true, minlength: 8},
-    price: {type: Number, required: true},
-    preptime: {type: Number, required: true},
-    description: {type: String, required: true},
-    tag: {type: String, required: true}
-}, {
-    timestamps: true,
-});
+const menuItemSchema = new Schema(
+  {
+    restaurantId: { type: String, required: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    preptime: { type: Number, required: true },
+    description: { type: String, required: true },
+    tag: { type: String },
+    category: { type: String },
+    isHidden: { type: Boolean, default: false }
+  },
+  {
+    timestamps: true
+  }
+);
 
-const menuItem = mongoose.model('menuItem', menuItemSchema);
+const menuItem = mongoose.model("menuItem", menuItemSchema);
 
 module.exports = menuItem;
