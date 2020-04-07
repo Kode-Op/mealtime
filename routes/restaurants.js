0187aaa8f07d21bf40b422c6b8a9f411c7569226
15davidallison <15david.allison@gmail.com>
2020-04-07 10:48:17 -0700

    Added API routes for restaurant tag lookup, deletion, addition, setting

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -6,8 +6,105 @@ let Restaurant = require("../models/restaurant_model");
 // Returns: All info on all restaurants
 router.route("/").get((req, res) => {
   Restaurant.find()
-    .then(restaurants => res.json(restaurants))
-    .catch(err => res.status(400).json("Error: " + err));
+    .then((restaurants) => res.json(restaurants))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// Format: GET /api/restaurants/byTag/:tag (tag is an int in range (1,42))
+// Required Fields: none
+// Returns: All info on restaurants containing corresponding tag
+router.route("/byTag/:tag").get((req, res) => {
+  let tag = Number(req.params.tag);
+  Restaurant.find({ tags: tag })
+    .then((restaurants) => res.json(restaurants))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// DEPRECATED - DO NOT USE (only for Insomnia use for migration, no max length check, limited error checks)
+// Format: POST /api/restaurants/addTags/:id
+// Required Fields: tags[]
+// Returns: List of successful/Unsuccsessful additions, prevents duplicates
+router.route("/addTags/:id").post((req, res) => {
+  let tagArray = req.body.tags;
+  let restaurantId = req.params.id;
+
+  Restaurant.findById(restaurantId)
+    .then((restaurant) => {
+      let origArray = restaurant.tags;
+      let errArray = [];
+      let sucArray = [];
+      for (i = 0; i < tagArray.length; i++) {
+        let exists = false;
+        for (j = 0; j < origArray.length; j++) {
+          if (tagArray[i] === origArray[j]) {
+            exists = true;
+          }
+        }
+        if (!exists) {
+          // if tag isn't already in the list
+          origArray.push(tagArray[i]);
+          sucArray.push(tagArray[i]);
+        } else {
+          // add to error list
+          errArray.push(tagArray[i]);
+        }
+      }
+
+      restaurant.tags = origArray;
+      let response = {
+        tagsAdded: sucArray,
+        alreadyInTags: errArray,
+        success: true,
+      };
+      restaurant
+        .save()
+        .then(() => res.json(response))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// Format: POST /api/restaurants/setTags/:id
+// Required Fields: tags[]
+// Returns: 200 on success, 413 if tag.length > 6, 404 if user not found, 400 if other error
+router.route("/setTags/:id").post((req, res) => {
+  let restaurantId = req.params.id;
+  let tags = req.body.tags;
+
+  if (tags.length > 6) {
+    return res.status(413).json("Too many tags, max length 6");
+  } else {
+    Restaurant.findById(restaurantId)
+      .then((restaurant) => {
+        restaurant.tags = tags;
+        restaurant
+          .save()
+          .then(() => res.json("Tags Set."))
+          .catch((err) => res.status(400).json("Error: " + err));
+      })
+      .catch((err) => res.status(400).json("Error: " + err));
+  }
+});
+
+// Format: GET /api/restaurants/clearTags/Restaurant._id
+// Required Fields: none
+// Returns: 404 if restaurant not found, 200 if tags reset, 400 if error
+router.route("/clearTags/:id").get((req, res) => {
+  let restaurantId = req.params.id;
+
+  Restaurant.findById(restaurantId)
+    .then((restaurant) => {
+      if (!restaurant) {
+        return res.status(404).json("Restaurant not found.");
+      } else {
+        restaurant.tags = [];
+        restaurant
+          .save()
+          .then(() => res.json("Tags Cleared"))
+          .catch((err) => res.status(400).json("Error: " + err));
+      }
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // Format: GET /api/restaurants/Restaurant._id
@@ -15,8 +112,8 @@ router.route("/").get((req, res) => {
 // Returns: All info on a specific restaurant
 router.route("/:id").get((req, res) => {
   Restaurant.findById(req.params.id)
-    .then(restaurants => res.json(restaurants))
-    .catch(err => res.status(400).json("Error: " + err));
+    .then((restaurants) => res.json(restaurants))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // Format: GET /api/restaurants/byOwner/User._id
@@ -24,8 +121,8 @@ router.route("/:id").get((req, res) => {
 // Returns: All info on restaurants owned by a particular owner
 router.route("/byOwner/:id").get((req, res) => {
   Restaurant.find({ ownerId: req.params.id })
-    .then(restaurants => res.json(restaurants))
-    .catch(err => res.status(400).json("Error: " + err));
+    .then((restaurants) => res.json(restaurants))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // Format: POST /api/restaurants/add
@@ -49,13 +146,13 @@ router.route("/add").post((req, res) => {
     minorder,
     address,
     hoursofoperation,
-    ownerId
+    ownerId,
   });
 
   newItem
     .save()
     .then(() => res.json("Restaurant added! Id: " + newItem.id))
-    .catch(err => res.status(400).json("Error: " + err));
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // DEPRECATED - DO NOT USE (only for Insomnia use for migration, very little error checking)
@@ -66,14 +163,14 @@ router.route("/addOwner/:id").post((req, res) => {
   restaurantId = req.params.id;
   ownerId = req.body.ownerId;
   Restaurant.findById(restaurantId)
-    .then(restaurants => {
+    .then((restaurants) => {
       restaurants.ownerId = ownerId;
       restaurants
         .save()
         .then(() => res.json("OwnerId updated."))
-        .catch(err => res.status(400).json("Error: " + err));
+        .catch((err) => res.status(400).json("Error: " + err));
     })
-    .catch(err => res.status(400).json("Error: " + err));
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
