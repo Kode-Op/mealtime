4e6c212e96536e3f5ce97aaf1c3b204edb7230ff
June Lee <juneslee97@email.com>
2020-04-09 12:25:02 -0700

    Added method to show tag images in RestaurantList.js

--- client/src/assets/displaytag/DisplayTag.js
+++ client/src/assets/displaytag/DisplayTag.js
@@ -41,97 +41,100 @@ import Thai from "../../assets/images/tags/38.jpg";
 import Vegan from "../../assets/images/tags/39.jpg";
 import Vegetarian from "../../assets/images/tags/40.jpg";
 import Vietnamese from "../../assets/images/tags/41.jpg";
+import Placeholder from "../../assets/images/tags/41.jpg";
 
 export default class DisplayTag extends Component {
   render() {
-    switch (this.props.tag) {
-      case 0:
-        return <img src={American} alt="0" />;
-      case 1:
-        return <img src={Asian} alt="1" />;
-      case 2:
-        return <img src={Bakery} alt="2" />;
-      case 3:
-        return <img src={BBQ} alt="3" />;
-      case 4:
-        return <img src={Breakfast} alt="4" />;
-      case 5:
-        return <img src={Brunch} alt="5" />;
-      case 6:
-        return <img src={Burgers} alt="6" />;
-      case 7:
-        return <img src={Cajun} alt="7" />;
-      case 8:
-        return <img src={Chicken} alt="8" />;
-      case 9:
-        return <img src={Chinese} alt="9" />;
-      case 10:
-        return <img src={Dessert} alt="10" />;
-      case 11:
-        return <img src={Dinner} alt="11" />;
-      case 12:
-        return <img src={Fastfood} alt="12" />;
-      case 13:
-        return <img src={Greek} alt="13" />;
-      case 14:
-        return <img src={Healthy} alt="14" />;
-      case 15:
-        return <img src={Indian} alt="15" />;
-      case 16:
-        return <img src={Italian} alt="16" />;
-      case 17:
-        return <img src={Japanese} alt="17" />;
-      case 18:
-        return <img src={Korean} alt="18" />;
-      case 19:
-        return <img src={Lunch} alt="19" />;
-      case 20:
-        return <img src={Mexican} alt="20" />;
-      case 21:
-        return <img src={Noodles} alt="21" />;
-      case 22:
-        return <img src={Pasta} alt="22" />;
-      case 23:
-        return <img src={Pizza} alt="23" />;
-      case 24:
-        return <img src={Ramen} alt="24" />;
-      case 25:
-        return <img src={Rich} alt="25" />;
-      case 26:
-        return <img src={Salalds} alt="26" />;
-      case 27:
-        return <img src={Salty} alt="27" />;
-      case 28:
-        return <img src={Sandwiches} alt="28" />;
-      case 29:
-        return <img src={Savory} alt="29" />;
-      case 30:
-        return <img src={Seafood} alt="30" />;
-      case 31:
-        return <img src={Singaporean} alt="31" />;
-      case 32:
-        return <img src={Soups} alt="32" />;
-      case 33:
-        return <img src={Southern} alt="33" />;
-      case 34:
-        return <img src={Spicy} alt="34" />;
-      case 35:
-        return <img src={Steak} alt="35" />;
-      case 36:
-        return <img src={Sweet} alt="36" />;
-      case 37:
-        return <img src={Tacos} alt="37" />;
-      case 38:
-        return <img src={Thai} alt="38" />;
-      case 39:
-        return <img src={Vegan} alt="39" />;
-      case 40:
-        return <img src={Vegetarian} alt="40" />;
-      case 41:
-        return <img src={Vietnamese} alt="41" />;
+    if (this.props.size === "small") {
+      switch (this.props.tag) {
+        case 0:
+          return <img src={American} alt="0" />;
+        case 1:
+          return <img src={Asian} alt="1" />;
+        case 2:
+          return <img src={Bakery} alt="2" />;
+        case 3:
+          return <img src={BBQ} alt="3" />;
+        case 4:
+          return <img src={Breakfast} alt="4" />;
+        case 5:
+          return <img src={Brunch} alt="5" />;
+        case 6:
+          return <img src={Burgers} alt="6" />;
+        case 7:
+          return <img src={Cajun} alt="7" />;
+        case 8:
+          return <img src={Chicken} alt="8" />;
+        case 9:
+          return <img src={Chinese} alt="9" />;
+        case 10:
+          return <img src={Dessert} alt="10" />;
+        case 11:
+          return <img src={Dinner} alt="11" />;
+        case 12:
+          return <img src={Fastfood} alt="12" />;
+        case 13:
+          return <img src={Greek} alt="13" />;
+        case 14:
+          return <img src={Healthy} alt="14" />;
+        case 15:
+          return <img src={Indian} alt="15" />;
+        case 16:
+          return <img src={Italian} alt="16" />;
+        case 17:
+          return <img src={Japanese} alt="17" />;
+        case 18:
+          return <img src={Korean} alt="18" />;
+        case 19:
+          return <img src={Lunch} alt="19" />;
+        case 20:
+          return <img src={Mexican} alt="20" />;
+        case 21:
+          return <img src={Noodles} alt="21" />;
+        case 22:
+          return <img src={Pasta} alt="22" />;
+        case 23:
+          return <img src={Pizza} alt="23" />;
+        case 24:
+          return <img src={Ramen} alt="24" />;
+        case 25:
+          return <img src={Rich} alt="25" />;
+        case 26:
+          return <img src={Salalds} alt="26" />;
+        case 27:
+          return <img src={Salty} alt="27" />;
+        case 28:
+          return <img src={Sandwiches} alt="28" />;
+        case 29:
+          return <img src={Savory} alt="29" />;
+        case 30:
+          return <img src={Seafood} alt="30" />;
+        case 31:
+          return <img src={Singaporean} alt="31" />;
+        case 32:
+          return <img src={Soups} alt="32" />;
+        case 33:
+          return <img src={Southern} alt="33" />;
+        case 34:
+          return <img src={Spicy} alt="34" />;
+        case 35:
+          return <img src={Steak} alt="35" />;
+        case 36:
+          return <img src={Sweet} alt="36" />;
+        case 37:
+          return <img src={Tacos} alt="37" />;
+        case 38:
+          return <img src={Thai} alt="38" />;
+        case 39:
+          return <img src={Vegan} alt="39" />;
+        case 40:
+          return <img src={Vegetarian} alt="40" />;
+        case 41:
+          return <img src={Vietnamese} alt="41" />;
 
-      default:
-        return <img src={} alt="" />;
+        default:
+          return <img src={Placeholder} alt="" />;
+      }
     }
   }
 }
