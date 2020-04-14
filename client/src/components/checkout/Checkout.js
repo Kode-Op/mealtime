37c444656b17f3f7fa6613416fc29086b90f7c4f
Rachel Scherer <rscherer1@student.rccd.edu>
2020-04-13 17:50:30 -0700

    Started creating checkout page

--- client/src/components/checkout/Checkout.js
+++ client/src/components/checkout/Checkout.js
@@ -3,20 +3,28 @@ import React, { Component } from "react";
 
 //Import components
 import Navbar from "../nav/Navbar";
+import Footer from "../footer/Footer";
+
+//Import assets
+import Loader from "../../assets/loader/Loader";
 
 //Import utilities
 import GetLogin from "../../utils/GetLogin";
 
+//Import stylesheets
+import "./Checkout.css";
+
 export default class Checkout extends Component {
+  _isMounted = false;
+
   constructor(props) {
     super(props);
-    this.state = {
-      isUserLoaded: false,
-      user: [],
-    };
+    this.state = {};
   }
 
   componentDidMount() {
+    this._isMounted = true;
+
     //Get "user" and "isUserLoaded" from the GetLogin utility
     GetLogin()
       .then((response) => {
@@ -35,12 +43,35 @@ export default class Checkout extends Component {
         }
       });
   }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
   render() {
-    return (
-      <div>
-        <Navbar />
-        <div>Hello, world!</div>
-      </div>
-    );
+    if (this.state.isUserLoaded) {
+      if (this.state.user) {
+        return (
+          <div>
+            <Navbar user={this.state.user} />
+            <div className="CheckoutContainer">
+              <div className="CheckoutBarMain">This goes on the left!</div>
+              <div className="CheckoutBarRight">This goes on the right!</div>
+            </div>
+            <Footer />
+          </div>
+        );
+      } else {
+        //User is not logged in
+        return (
+          <div>
+            <Navbar />
+            TODO
+          </div>
+        );
+      }
+    } else {
+      return <Loader />;
+    }
   }
 }
