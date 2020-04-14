5e5a3938549172337f512398b419699b3543ef41
15davidallison <15david.allison@gmail.com>
2020-04-13 17:50:53 -0700

    Finished Order Schema, Model + testing

--- models/order_model.js
+++ models/order_model.js
@@ -1,32 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 let MenuItem = require("./menuItem_model");
 
 const Schema = mongoose.Schema;
 
-const orderSchema = new Schema({
-    userID: {type: String, required: true},
-    restaurantID: {type: String, required: true},
-    menuItems:{type: Array, required:true},
-    quantityMenuItems:{type: Array, required:true, items:{type:Number}},
-    prepTime:{type:Number, required:true},
-    deliverylocation: {
-        type: String,
-        required:true
-    },
-    isCanceled:{type:Boolean, default:false},
-    isFulfilled:{type:Boolean, default:false}
-    }, 
-{
+const orderSchema = new Schema(
+  {
+    userId: { type: String, required: true },
+    restaurantId: { type: String, required: true },
+    creditCardId: { type: String, required: true },
+    menuItems: { type: Array, required: true },
+    prepTime: { type: Number },
+    address: { type: String, required: true },
+    isCanceled: { type: Boolean, required: true, default: false },
+    isFulfilled: { type: Boolean, required: true, default: false },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-orderSchema.methods.orderDone = function(){
-    if(!isCanceled){
-        isFulfilled = true;
-    }
-    return isFulfilled;
+orderSchema.methods.orderDone = function () {
+  if (!isCanceled) {
+    isFulfilled = true;
+  }
+  return isFulfilled;
 };
 
-const order = mongoose.model('order', orderSchema);
+const order = mongoose.model("order", orderSchema);
 
 module.exports = order;
--- routes/orders.js
+++ routes/orders.js
@@ -1,83 +1,98 @@
-const router = require('express').Router();
-let Order = require('../models/order_model');
+const router = require("express").Router();
+let Order = require("../models/order_model");
+let MenuItem = require("../models/menuItem_model");
 
 // Format: GET /api/orders/
 // Required Fields: none
 // Returns: All info on all orders
-router.route('/').get((req, res) => {
-    Order.find()
-        .then(orders => res.json(orders))
-        .catch(err => res.status(400).json('Error: ' + err));
+router.route("/").get((req, res) => {
+  Order.find()
+    .then((orders) => res.json(orders))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-// Format: GET /api/userOrders/User._id
+// Format: GET /api/orders/byUser/User._id
 // Required Fields: none
 // Returns: All orders on a specific user
-router.route('/userOrders/:id').get((req, res) => {
-    Order.find({userID:req.params.id})
-        .then(orders => res.json(orders))
-        .catch(err => res.status(400).json('Error: ' + err));
+router.route("/byUser/:id").get((req, res) => {
+  Order.find({ userId: req.params.id })
+    .then((orders) => res.json(orders))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-// Format: GET /api/resturantOrders/Resturant._id
+// Format: GET /api/orders/byRestaurant/Resturant._id
 // Required Fields: none
 // Returns: All orders on a specific resturant
-router.route('/resturantOrders/:id').get((req, res) => {
-    Order.find({restaurantID:req.params.id})
-        .then(orders => res.json(orders))
-        .catch(err => res.status(400).json('Error: ' + err));
+router.route("/byRestaurant/:id").get((req, res) => {
+  Order.find({ restaurantId: req.params.id })
+    .then((orders) => res.json(orders))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req,res) => {
-    const userID = req.body.userID;
-    const restaurantID = req.body.restaurantID;
-    const menuItems = req.body.menuItems;
-    const quantityMenuItems = req.body.quantityMenuItems;
-    const prepTime = req.body.prepTime;
-    const deliverylocation = req.body.deliverylocation;
-    const isCanceled = req.body.isCanceled;
-    const isFulfilled = req.body.isFulfilled;
+// Format: POST /api/orders/add
+// Required Fields: userId, restaurantId, creditCardId, menuItems[], address
+// Returns: Status based on successful/unsuccessful order creation
+router.route("/add").post((req, res) => {
+  const userId = req.body.userId;
+  const restaurantId = req.body.restaurantId;
+  const creditCardId = req.body.creditCardId;
+  const menuItems = req.body.menuItems;
+  const address = req.body.address;
+  let prepTime = 0;
 
-    const newItem = new Order({
-        userID,
-        restaurantID,
-        menuItems,
-        quantityMenuItems,
-        prepTime,
-        deliverylocation,
-        isCanceled,
-        isFulfilled
+  const findTime = (id) => {
+    return MenuItem.findById(id)
+      .then((item) => {
+        prepTime += item.preptime;
+      })
+      .catch((err) => res.status(400).json("Error: " + err));
+  };
+
+  const createOrder = async (_) => {
+    for (let i = 0; i < menuItems.length; i++) {
+      const result = await findTime(menuItems[i]);
+    }
+
+    const newOrder = new Order({
+      userId,
+      restaurantId,
+      creditCardId,
+      menuItems,
+      prepTime,
+      address,
     });
 
-    setTimeOut(newItem.orderDone(),60000*prepTime);
+    newOrder
+      .save()
+      .then(() => res.json("Order added!"))
+      .catch((err) => res.status(400).json("Error: " + err));
+  };
 
-    newItem.save()
-        .then(() => res.json('Order added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+  createOrder();
 });
 
 // Format: GET /api/orders/Order._id
 // Required Fields: none
 // Returns: All info on a specific order
 router.route("/:id").get((req, res) => {
-    Order.findById(req.params.id)
-      .then((orders) => res.json(orders))
-      .catch((err) => res.status(400).json("Error: " + err));
+  Order.findById(req.params.id)
+    .then((orders) => res.json(orders))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-// Format: Post /api/orders/cancel/Order._id
+// Format: GET /api/orders/cancel/Order._id
 // Required Fields: none
 // Returns: All info on a specific order
-router.route("/cancel/:id").post((req, res) => {
-    Order.findById(req.params.id)
-      .then((orders) =>{
-            orders.isCanceled = true;
-        orders
-            .save()
-            .then(() => res.json("Order Cancelled."))
-            .catch((err) => res.status(400).json("Error: " + err));
-      })
-      .catch((err) => res.status(400).json("Error: " + err));
+router.route("/cancel/:id").get((req, res) => {
+  Order.findById(req.params.id)
+    .then((orders) => {
+      orders.isCanceled = true;
+      orders
+        .save()
+        .then(() => res.json("Order Cancelled."))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
