0ce167ceb426371dc3d9b2cc1f6b80741f6043ba
Rachel Scherer <rscherer1@student.rccd.edu>
2020-04-21 14:27:19 -0700

    Finished order page, added overlay to checkout when user isn't logged in

--- client/src/components/order/Order.js
+++ client/src/components/order/Order.js
@@ -1,11 +1,11 @@
 //Import libraries
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 import axios from "axios";
 
 //Import components
 import Navbar from "../nav/Navbar";
-// import Footer from "../footer/Footer";
+import Footer from "../footer/Footer";
 
 //Import assets
 import FoodImg from "../../assets/images/orders/food.png";
@@ -33,42 +33,19 @@ export default class Order extends Component {
       user: [],
       isOrderLoaded: false,
       isUserLoaded: false,
+      readyTime: "",
     };
   }
 
   componentDidMount() {
     this._isMounted = true;
 
+    //Bind 'this' to loadOrder()
+    this.loadOrder = this.loadOrder.bind(this);
+
     if (this.state.id !== "") {
-      axios
-        .get("/api/orders/" + this.state.id)
-        .then((response) => {
-          this.setState({
-            order: response.data,
-            isOrderLoaded: true,
-          });
-          let date = new Date(response.data.createdAt);
-          console.log(
-            date.getMinutes(
-              date.setMinutes(date.getMinutes() + response.data.prepTime)
-            )
-          );
-          //date.setMinutes(date.getMinutes() + response.data.preptime);
-          //let isoDate = date.toISOString();
-          console.log(date);
-          //console.log(isoDate);
-        })
-        .catch(() => {
-          this.setState({
-            order: null,
-            isOrderLoaded: true,
-          });
-        });
-    } else {
-      this.setState({
-        order: null,
-        isOrderLoaded: true,
-      });
+      this.loadOrder();
+      setInterval(this.loadOrder, 30000);
     }
 
     //Get "user" and "isUserLoaded" from the GetLogin utility
@@ -90,8 +67,46 @@ export default class Order extends Component {
       });
   }
 
-  getOrderStatus = (order) => {
+  //This functions gets the order data. Set on a 30 second timer
+  async loadOrder() {
+    return axios
+      .get("/api/orders/" + this.state.id)
+      .then((response) => {
+        //Convert createdAt from an ISO Date format to the JavaScript standard string format
+        let readyTime = new Date(response.data[0].createdAt);
+
+        //Add the prepTime to the createdAt date to get readyTime
+        readyTime.setMinutes(
+          readyTime.getMinutes() + response.data[0].prepTime
+        );
+
+        this.setState({
+          order: response.data[0],
+          isOrderLoaded: true,
+          readyTime: readyTime,
+        });
+      })
+      .catch(() => {
+        this.setState({
+          order: null,
+          isOrderLoaded: true,
+        });
+      });
+  }
+
+  getOrderStatus = (order, readyTime) => {
     const { isCancelled, isFulfilled } = order;
+
+    const currentTime = new Date();
+    const timeDifference = readyTime - currentTime;
+    let minutesRemaining;
+
+    if (timeDifference > 0) {
+      minutesRemaining = new Date(timeDifference).getMinutes();
+    } else {
+      minutesRemaining = 0;
+    }
+
     if (isCancelled) {
       return (
         <div>
@@ -102,25 +117,40 @@ export default class Order extends Component {
     } else if (isFulfilled) {
       return (
         <div>
-          <h3>Your order has been fulfilled!</h3>
+          <h3>Your order has been fulfilled. Enjoy!</h3>
           <br />
         </div>
       );
     } else {
-      return (
-        <div>
-          <h3>Your order is being processed!</h3>
-          <p>
-            Estimated time remaining:{" "}
-            <span style={{ color: "green" }}>4 minutes</span>
-          </p>
-        </div>
-      );
+      if (minutesRemaining > 0) {
+        return (
+          <div>
+            <h3>Your order is being processed!</h3>
+            <p>
+              Estimated time remaining:{" "}
+              <span style={{ color: "green" }}>
+                About {minutesRemaining} minute
+                {minutesRemaining !== 1 ? "s" : ""}
+              </span>
+            </p>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <h3>Your order is being processed!</h3>
+            <p>
+              Estimated time remaining:{" "}
+              <span style={{ color: "green" }}>Any minute now</span>
+            </p>
+          </div>
+        );
+      }
     }
   };
   render() {
     //Deconstruct the state
-    const { isUserLoaded, user, isOrderLoaded, order } = this.state;
+    const { isUserLoaded, user, isOrderLoaded, order, readyTime } = this.state;
 
     if (isUserLoaded && isOrderLoaded) {
       if (!user) {
@@ -131,17 +161,30 @@ export default class Order extends Component {
           <div>
             <Navbar user={user} />
             <div className="OrderCenterDiv">
-              {this.getOrderStatus(order)}
+              {this.getOrderStatus(order, readyTime)}
               <img src={FoodImg} alt="" />
+              <br />
+              <br />
               <div>Delivering to: {order.custAddress} </div>
             </div>
+            <Footer />
           </div>
         );
       } else {
         return (
           <div>
             <Navbar user={user} />
-            Invalid order
+            <br />
+            <br />
+            <h3>Oops</h3>
+            <br />
+            <p>
+              We weren't able to find the order you are looking for. Sorry about
+              that!
+            </p>
+            <p>
+              Click <Link to="/">here</Link> to return home.
+            </p>
           </div>
         );
       }
